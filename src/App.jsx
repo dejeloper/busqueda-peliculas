6dd52e4adad8db17b3f44988ad94948425e3273a
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-06-13 23:04:39 -0500

    Add Form and globals styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import './App.css'
 
 const App = () => {
   return (
-    <>
-      <h1>Búsqueda de Películas</h1>
-    </>
+    <div className='page'>
+      <header>
+        <h1>Búsqueda de Películas</h1>
+        <form className='form'>
+          <input placeholder='Avengers, Star Wars, Matrix...' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+      <main>
+        <p>Resultados</p>
+      </main>
+    </div>
   )
 }
 

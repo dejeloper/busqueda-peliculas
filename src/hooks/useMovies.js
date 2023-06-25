b5d2bed47fe1c17e4962aa8ccd244172f55f3bcb
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-06-24 23:40:40 -0500

    Add useRef to prevent re-render for the same search

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export const useMovies = ({ search }) => {
   const [ movies, setMovies ] = useState([])
   const [ loading, setLoading ] = useState(false)
   const [ errorSearch, setErrorSearch ] = useState(null)
+  const previousSearch = useRef(search)
 
   const getMovies = async () => {
+    if (search === previousSearch.current) return
+
     try {
       setLoading(true)
       setErrorSearch(null)
+      previousSearch.current = search
 
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
